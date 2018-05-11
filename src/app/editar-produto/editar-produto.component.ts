c8025bbe3b856a3782850cfbcf221db44d226e56
Leonardo Godinho <godinho.leo@gmail.com>
2018-05-10 22:38:56 -0300

    Data Binding

--- src/app/editar-produto/editar-produto.component.ts
+++ src/app/editar-produto/editar-produto.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Produto } from '../produto';
 
 @Component({
   selector: 'app-editar-produto',
@@ -8,12 +9,30 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class EditarProdutoComponent implements OnInit {
 
+  produto: Produto;
+
   constructor(
     private route: ActivatedRoute
   ) { }
 
   ngOnInit() {
-    this.route.snapshot.params['id']
+    //this.route.snapshot.params['id']
+
+    this.produto = new Produto();
+    this.produto.id = 1;
+    this.produto.nome = "Macarrão";
+  }
+
+  change() {
+    alert('Change');
+  }
+
+  salvar() {
+    alert(this.produto.nome);
+  }
+
+  get debug() {
+    return JSON.stringify(this.produto);
   }
 
 }
