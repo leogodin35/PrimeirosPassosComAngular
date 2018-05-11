703d9f2f531fe608d11b09e114d90d52fa584817
Leonardo Godinho <godinho.leo@gmail.com>
2018-05-10 23:03:13 -0300

    Diretivas

--- src/app/produtos/produtos.component.ts
+++ src/app/produtos/produtos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Produto } from '../produto';
 
 @Component({
   selector: 'app-produtos',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./produtos.component.css']
 })
 export class ProdutosComponent implements OnInit {
+  produtos: Produto[] = [];
 
   constructor() { }
 
   ngOnInit() {
+    let produto = new Produto();
+    produto.id = 1;
+    produto.nome = "Macarrão";
+    this.produtos.push(produto);
+
+    let produto2 = new Produto();
+    produto2.id = 2;
+    produto2.nome = "Feijão";
+    this.produtos.push(produto2);
   }
 
 }
